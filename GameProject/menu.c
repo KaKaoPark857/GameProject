174630c8302141b8cf9b8de0a8679a7064dd91f0
psw3732 <49482420+psw3732@users.noreply.github.com>
2021-07-04 09:26:05 +0900

    change code

--- GameProject/menu.c
+++ GameProject/menu.c
@@ -7,42 +7,54 @@ int menu() {
 	int choiceMenu;
 	while (1) {
 		textcolor(14);
-		printf("\t\t\t\t\t\t\t\t**********************************************\n");
-		printf("\t\t\t\t\t\t\t\t==================미니게임 천국===============\n");
-		printf("\t\t\t\t\t\t\t\t1. 가위바위보\t2. 3 6 9 게임\t3. 숫자 야구\n");
-		printf("\t\t\t\t\t\t\t\t4. 업엔 다운\t5. 주사위 굴리기\t\n");
-		printf("\t\t\t\t\t\t\t\t==============================================\n");
-		printf("\t\t\t\t\t\t\t\t**********************************************\n");
+		printf("\t\t\t\t\t\t\t\t");
+		printf("**********************************************\n");
+		printf("\t\t\t\t\t\t\t\t");
+		printf("==================미니게임 천국===============\n");
+		printf("\t\t\t\t\t\t\t\t");
+		printf("1. 가위바위보\t2. 3 6 9 게임\t3. 숫자 야구\n");
+		printf("\t\t\t\t\t\t\t\t");
+		printf("4. 업엔 다운\t5. 주사위 굴리기\t\n");
+		printf("\t\t\t\t\t\t\t\t");
+		printf("==============================================\n");
+		printf("\t\t\t\t\t\t\t\t");
+		printf("**********************************************\n");
 		printf("\n\t\t\t\t\t\t\t\t번호 입력(종료 0) : ");
 		scanf_s("%d", &choiceMenu);
 		system("cls");
 		switch (choiceMenu) {
 		case 1: 
+			Select1();
 			textcolor(11);
 			RSP_Game();
 			system("cls");
 			break;
 		case 2: 
+			Select2();
 			textcolor(11);
 			ThreeTime();
 			system("cls");
 			break;
 		case 3: 
+			Select3();
 			textcolor(11);
 			//baseBall();
 			system("cls");
 			break;
 		case 4: 
+			Select4();
 			textcolor(11);
 			UpnDown();
 			system("cls");
 			break;
 		case 5: 
+			Select5();
 			textcolor(11);
 			Dice();
 			system("cls");
 			break;
 		case 6: 
+			Select6();
 			textcolor(11);
 			//blackJack or 섯다
 			break;
@@ -53,5 +65,5 @@ END:
 	return 0;
 }
 
-//warning 부분 글자 빨간색 게임의 전체적인 흰배경 검정 글씨 게임 메뉴에서 게임 선택시 system("cls") 사용 해당 글씨만 빨간색 Sleep(500)에 해당 게임 접속
+//게임의 전체적인 흰배경 검정 글씨 게임 메뉴에서 게임 선택시 system("cls") 사용 해당 글씨만 빨간색 Sleep(500)에 해당 게임 접속
 // 
---
+++ GameProject/munuSelect.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+
+//메뉴 선택 시 나오는 효과
+
+void Select1() {
+	printf("\t\t\t\t\t\t\t\t");
+	printf("**********************************************\n");
+	printf("\t\t\t\t\t\t\t\t");
+	printf("==================미니게임 천국===============\n");
+	printf("\t\t\t\t\t\t\t\t"); textcolor(12);
+	printf("1. 가위바위보\t"); textcolor(14);
+	printf("2. 3 6 9 게임\t");
+	printf("3. 숫자 야구\n");
+	printf("\t\t\t\t\t\t\t\t");
+	printf("4. 업엔 다운\t");
+	printf("5. 주사위 굴리기\t\n");
+	printf("\t\t\t\t\t\t\t\t");
+	printf("==============================================\n");
+	printf("\t\t\t\t\t\t\t\t");
+	printf("**********************************************\n");
+}
+
+void Select2() {
+	printf("\t\t\t\t\t\t\t\t");
+	printf("**********************************************\n");
+	printf("\t\t\t\t\t\t\t\t");
+	printf("==================미니게임 천국===============\n");
+	printf("\t\t\t\t\t\t\t\t"); 
+	printf("1. 가위바위보\t"); textcolor(12); 
+	printf("2. 3 6 9 게임\t"); textcolor(14);
+	printf("3. 숫자 야구\n");
+	printf("\t\t\t\t\t\t\t\t");
+	printf("4. 업엔 다운\t");
+	printf("5. 주사위 굴리기\t\n");
+	printf("\t\t\t\t\t\t\t\t");
+	printf("==============================================\n");
+	printf("\t\t\t\t\t\t\t\t");
+	printf("**********************************************\n");
+}
+
+void Select3() {
+	printf("\t\t\t\t\t\t\t\t");
+	printf("**********************************************\n");
+	printf("\t\t\t\t\t\t\t\t");
+	printf("==================미니게임 천국===============\n");
+	printf("\t\t\t\t\t\t\t\t"); 
+	printf("1. 가위바위보\t"); 
+	printf("2. 3 6 9 게임\t"); textcolor(12);
+	printf("3. 숫자 야구\n"); textcolor(14);
+	printf("\t\t\t\t\t\t\t\t");
+	printf("4. 업엔 다운\t");
+	printf("5. 주사위 굴리기\t\n");
+	printf("\t\t\t\t\t\t\t\t");
+	printf("==============================================\n");
+	printf("\t\t\t\t\t\t\t\t");
+	printf("**********************************************\n");
+}
+
+void Select4() {
+	printf("\t\t\t\t\t\t\t\t");
+	printf("**********************************************\n");
+	printf("\t\t\t\t\t\t\t\t");
+	printf("==================미니게임 천국===============\n");
+	printf("\t\t\t\t\t\t\t\t"); 
+	printf("1. 가위바위보\t"); 
+	printf("2. 3 6 9 게임\t");
+	printf("3. 숫자 야구\n"); textcolor(12);
+	printf("\t\t\t\t\t\t\t\t");
+	printf("4. 업엔 다운\t"); textcolor(14);
+	printf("5. 주사위 굴리기\t\n");
+	printf("\t\t\t\t\t\t\t\t");
+	printf("==============================================\n");
+	printf("\t\t\t\t\t\t\t\t");
+	printf("**********************************************\n");
+}
+
+void Select5() {
+	printf("\t\t\t\t\t\t\t\t");
+	printf("**********************************************\n");
+	printf("\t\t\t\t\t\t\t\t");
+	printf("==================미니게임 천국===============\n");
+	printf("\t\t\t\t\t\t\t\t"); 
+	printf("1. 가위바위보\t"); 
+	printf("2. 3 6 9 게임\t");
+	printf("3. 숫자 야구\n");
+	printf("\t\t\t\t\t\t\t\t");
+	printf("4. 업엔 다운\t"); textcolor(12);
+	printf("5. 주사위 굴리기\t\n"); textcolor(14);
+	printf("\t\t\t\t\t\t\t\t");
+	printf("==============================================\n");
+	printf("\t\t\t\t\t\t\t\t");
+	printf("**********************************************\n");
+}
+
+void Select6() {
+	printf("\t\t\t\t\t\t\t\t");
+	printf("**********************************************\n");
+	printf("\t\t\t\t\t\t\t\t");
+	printf("==================미니게임 천국===============\n");
+	printf("\t\t\t\t\t\t\t\t"); textcolor(12);
+	printf("1. 가위바위보\t"); textcolor(14);
+	printf("2. 3 6 9 게임\t");
+	printf("3. 숫자 야구\n");
+	printf("\t\t\t\t\t\t\t\t");
+	printf("4. 업엔 다운\t");
+	printf("5. 주사위 굴리기\t\n");
+	printf("\t\t\t\t\t\t\t\t");
+	printf("==============================================\n");
+	printf("\t\t\t\t\t\t\t\t");
+	printf("**********************************************\n");
+}
